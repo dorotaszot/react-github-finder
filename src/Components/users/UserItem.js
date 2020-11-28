2ff9c95275c00dd6b552d4445d7e07fbfd0ff213
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-11-28 12:19:47 +0100

    class based component, states added, single user hard coded added

--- src/Components/users/UserItem.js
+++ src/Components/users/UserItem.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
+  state = {
+    id: 'id',
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://api.github.com/users/mojombo'
+  }
+
   render() {
+    const {login, avatar_url, html_url} = this.state;
+
     return (
-      <div>
-        UserItem
+      <div className="card text-center">
+        <img src={avatar_url} alt="" className="img-rounded" style={{width:"60px"}}/>
+        <p className="my-sm">{login}</p>
+        <div>
+
+        <a href={html_url} className="btn btn-dark p-sm">More</a>
+        </div>
       </div>
     )
   }
