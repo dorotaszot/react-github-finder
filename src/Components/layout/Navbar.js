eafec1f42371c864f6d775bec26f3c1fe78bf668
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-11-29 20:55:35 +0100

    class components UserItem and Navbar changed to stateless functional components

--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-
-export class Navbar extends Component {
-  static defaultProps = {
+const Navbar = ({icon, title}) => {
+    return (
+      <nav className="p-sm red-background white">
+        <h1>
+    <i className={icon} ></i> {title}
+        </h1>
+      </nav>
+    )
+ 
+  }
+  Navbar.defaultProps = {
     title: 'Github Finder',
     icon: 'fab fa-github'
   }
 
-  static propTypes = {
+  Navbar.propTypes = {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired
   }
 
-  render() {
-    return (
-      <nav className="p-sm red-background white">
-        <h1>
-    <i className={this.props.icon} ></i> {this.props.title}
-        </h1>
-      </nav>
-    )
-  }
-}
 
 export default Navbar
