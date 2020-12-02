a3fa0c03a080b93d00fb5eaeab2e2442983a069d
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-12-02 14:07:27 +0100

    clear button added

--- src/Components/users/Search.js
+++ src/Components/users/Search.js
@@ -8,10 +8,11 @@ state = {
 onSubmit = (e) => {
   e.preventDefault();
   this.props.searchUsers(this.state.text);
+  this.setState({ text: '' });
 }
 
 onChange = (e) => {
-  this.setState({[e.target.name]: e.target.value})
+  this.setState({[e.target.name]: e.target.value});
 }
 
   render() {
@@ -21,6 +22,12 @@ onChange = (e) => {
           <input type="text" name="text" placeholder="Search Users..." className="my-sm p-sm" value={this.state.text} onChange={this.onChange}/>
           <input type="submit" value="Search" className="btn btn-dark btn-block p-sm" />
         </form>
+        {this.props.showClear && 
+          <button className="btn btn-block btn-light p-sm my-sm" style={{ color: '#000'}} onClick={this.props.clearSearch}>
+            Clear
+          </button>
+        }
+    
       </div>
     )
   }
